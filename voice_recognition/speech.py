ca5a81c0f45d3256ddb149a5d2c3e1f336b405a3
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-05-13 16:57:25 +0200

    Better voice syncing

--- voice_recognition/speech.py
+++ voice_recognition/speech.py
@@ -21,7 +21,7 @@ def transcribe_audio_file(file_path: str):
     # Extract the word timings
     word_timings = list()
     for word in transcription.seg():
-        word_timings.append((word.word, word.start_frame / clip.fps, word.end_frame / clip.fps))
+        word_timings.append((word.word, word.start_frame / transcription.get_config()["frate"], word.end_frame / transcription.get_config()["frate"]))
 
     return word_timings, clip
 
@@ -33,9 +33,12 @@ def get_text_clip_from_audio(audio_path: str) -> VideoClip:
         if word != "<s>":
             subclip = TextClip(word, fontsize=30, color='white').set_fps(24)
             subclip = subclip.set_start(start_time)
-            subclip = subclip.set_end(end_time * 24 * 4)
+            subclip = subclip.set_end(end_time)
             subclips.append(subclip)
 
     final_clip = concatenate_videoclips(subclips, method='compose')
     final_clip = final_clip.set_audio(audio)
     return final_clip.set_duration(audio.duration)
+
+if __name__ == "__main__":
+    get_text_clip_from_audio("20.mp3").write_videofile("20_text.mp4")
